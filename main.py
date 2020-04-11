9e0ea985722cfd9541c3ec73be49eb083b66353e
byqgithub <1104963145@qq.com>
2020-04-11 11:42:11 +0800

    Add project code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ chrome_option = Options()
 chrome_option.add_argument("--headless")
 chrome_option.add_argument("--disable-gpu")
 driver = webdriver.Chrome(executable_path=r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe',
-                          options=None)
+                          options=chrome_option)
 driver.get("https://segmentfault.com/a/1190000015642808")
 try:
     current_web_id = driver.find_element_by_id("item-1")
